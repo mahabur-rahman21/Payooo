ee557884fff65afc6b888ca91429c2cefd38d8c9
mahabur-rahman21 <mahabursoikot04@gmail.com>
2025-02-28 17:54:20 +0600

    added section

---
+++ scripts/home.js
@@ -0,0 +1,129 @@
+let addMoney = document.getElementById("addMoney");
+let cashOut = document.getElementById("cashOut");
+let transferMoney = document.getElementById("transferMoney");
+let getBonus = document.getElementById("getBonus");
+let payBill = document.getElementById("payBill");
+let transactions = document.getElementById("transactions");
+
+
+
+addMoney.addEventListener("click", function (e) {
+
+    e.preventDefault
+
+    let addMoneySection=document.getElementById("add-Money-Section")
+    let cashOutSection=document.getElementById("cash-Out-Section")
+    let transferMoneySection=document.getElementById("transfer-Money-Section")
+    let getBonusSection=document.getElementById("get-Bonus-Section")
+    let payBillSection=document.getElementById("pay-Bill-Section")
+    let transactionHistorySection=document.getElementById("transaction-History-Section")
+
+    addMoneySection.style.display="block"
+    cashOutSection.style.display="none"
+    transferMoneySection.style.display="none"
+    getBonusSection.style.display="none"
+    payBillSection.style.display="none"
+    transactionHistorySection.style.display="none"
+
+    
+})
+cashOut.addEventListener("click", function (e) {
+
+    e.preventDefault
+
+    let addMoneySection=document.getElementById("add-Money-Section")
+    let cashOutSection=document.getElementById("cash-Out-Section")
+    let transferMoneySection=document.getElementById("transfer-Money-Section")
+    let getBonusSection=document.getElementById("get-Bonus-Section")
+    let payBillSection=document.getElementById("pay-Bill-Section")
+    let transactionHistorySection=document.getElementById("transaction-History-Section")
+
+    addMoneySection.style.display="none"
+    cashOutSection.style.display="block"
+    transferMoneySection.style.display="none"
+    getBonusSection.style.display="none"
+    payBillSection.style.display="none"
+    transactionHistorySection.style.display="none"
+
+    
+})
+transferMoney.addEventListener("click", function (e) {
+
+    e.preventDefault
+
+    let addMoneySection=document.getElementById("add-Money-Section")
+    let cashOutSection=document.getElementById("cash-Out-Section")
+    let transferMoneySection=document.getElementById("transfer-Money-Section")
+    let getBonusSection=document.getElementById("get-Bonus-Section")
+    let payBillSection=document.getElementById("pay-Bill-Section")
+    let transactionHistorySection=document.getElementById("transaction-History-Section")
+
+    addMoneySection.style.display="none"
+    cashOutSection.style.display="none"
+    transferMoneySection.style.display="block"
+    getBonusSection.style.display="none"
+    payBillSection.style.display="none"
+    transactionHistorySection.style.display="none"
+
+    
+})
+getBonus.addEventListener("click", function (e) {
+
+    e.preventDefault
+
+    let addMoneySection=document.getElementById("add-Money-Section")
+    let cashOutSection=document.getElementById("cash-Out-Section")
+    let transferMoneySection=document.getElementById("transfer-Money-Section")
+    let getBonusSection=document.getElementById("get-Bonus-Section")
+    let payBillSection=document.getElementById("pay-Bill-Section")
+    let transactionHistorySection=document.getElementById("transaction-History-Section")
+
+    addMoneySection.style.display="none"
+    cashOutSection.style.display="none"
+    transferMoneySection.style.display="none"
+    getBonusSection.style.display="block"
+    payBillSection.style.display="none"
+    transactionHistorySection.style.display="none"
+
+    
+})
+payBill.addEventListener("click", function (e) {
+
+    e.preventDefault
+
+    let addMoneySection=document.getElementById("add-Money-Section")
+    let cashOutSection=document.getElementById("cash-Out-Section")
+    let transferMoneySection=document.getElementById("transfer-Money-Section")
+    let getBonusSection=document.getElementById("get-Bonus-Section")
+    let payBillSection=document.getElementById("pay-Bill-Section")
+    let transactionHistorySection=document.getElementById("transaction-History-Section")
+
+    addMoneySection.style.display="none"
+    cashOutSection.style.display="none"
+    transferMoneySection.style.display="none"
+    getBonusSection.style.display="none"
+    payBillSection.style.display="block"
+    transactionHistorySection.style.display="none"
+
+    
+})
+transactions.addEventListener("click", function (e) {
+
+    e.preventDefault
+
+    let addMoneySection=document.getElementById("add-Money-Section")
+    let cashOutSection=document.getElementById("cash-Out-Section")
+    let transferMoneySection=document.getElementById("transfer-Money-Section")
+    let getBonusSection=document.getElementById("get-Bonus-Section")
+    let payBillSection=document.getElementById("pay-Bill-Section")
+    let transactionHistorySection=document.getElementById("transaction-History-Section")
+
+    addMoneySection.style.display="none"
+    cashOutSection.style.display="none"
+    transferMoneySection.style.display="none"
+    getBonusSection.style.display="none"
+    payBillSection.style.display="none"
+    transactionHistorySection.style.display="block"
+
+    
+})
